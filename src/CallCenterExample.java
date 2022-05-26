3dc1e4d56dcc24eec5fcc2f083168a934cdcd1e2
SergyProg <serj.shoping@yandex.ru>
2022-05-26 23:45:19 +0300

    АТС Вер.1

---
+++ src/CallCenterExample.java
@@ -0,0 +1,66 @@
+package ru.netology;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class CallCenterExample {
+    static final int NUMBER_OF_CALLS = 60;
+    static final int RESPONSE_TIME = 3000;
+    static final int INTERVAL_BETWEEN_CALLS = 1000;
+    static final int NUMBER_OF_SPECIALIST = 5;
+
+    static final String DONE = "End";
+
+    private BlockingQueue<String> drop;
+    volatile boolean stopThreads = false;
+
+    public CallCenterExample() {
+        drop = new ArrayBlockingQueue<String>(1, true);
+        (new Thread(new Calls())).start();
+        ThreadGroup specialistsGroup = new ThreadGroup("Specialists");
+        for (int i = 1; i <= NUMBER_OF_SPECIALIST; i++)
+            (new Thread(specialistsGroup, new Specialists())).start();
+    }
+
+    class Calls implements Runnable {
+        public void run() {
+            try {
+                for (int i = 1; i <= NUMBER_OF_CALLS; i++) {
+                    drop.put("звонок " + i);
+                    System.out.println("На АТС поступил звонок " + i);
+                    Thread.sleep(INTERVAL_BETWEEN_CALLS);
+                }
+                drop.put(DONE);
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    class Specialists implements Runnable {
+        public void run() {
+            String call = null;
+            try {
+               // Так и не понял почему подвисают потоки специалистов. По-возможности подскажите где упущение.
+               // условие while (true && !stopThreads) должно закрывать их (выходим из run) при достижении лимита звонков
+                while (true && !stopThreads) {
+                    if (drop.peek() != null) {
+                        call = drop.take();
+                        if (call == DONE) {
+                            stopThreads = true;
+                            break;
+                        }
+                        System.out.println("Специалист (" + Thread.currentThread().getName() + ") ответил на " + call);
+                        Thread.sleep(RESPONSE_TIME);
+                    }//else Thread.yield();
+                }
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new CallCenterExample();
+    }
+}
